35be02379534079216f196671d7ea487888933d1
rahyanakil <rahyanakil89@gmail.com>
2026-05-06 13:14:00 +0600

    fix:vercel data load issue 2

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,9 @@ const nextConfig: NextConfig = {
     ],
   },
   async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
     return [
-      { source: '/api/:path*', destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` },
+      { source: '/api/:path*', destination: `${backendUrl}/api/:path*` },
     ];
   },
 };
